6a0cc586eb68ceef05eea3f11c3a44fb5160ee39
Bayu Lingga <bylinggha@gmail.com>
2025-02-24 09:34:52 +0700

    manage admin terpisah

---
+++ resources/views/manageusers.blade.php
@@ -0,0 +1,42 @@
+@extends('master')
+@section('konten')
+<div class="container-fluid">
+    <div class="row">
+        <!-- Main content -->
+        <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <div class="card mt-4">
+                <div class="card-header">
+                    <h3 class="card-title">Manage Users</h3>
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($users as $user)
+                            <tr>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>
+                                    <form action="{{ route('manage.destroy', $user->id) }}" 
+                                          method="POST" class="d-inline">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-sm btn-danger">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
